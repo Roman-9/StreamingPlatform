56cb96a4525c996baad70f94dda04c4ab528fbed
Roman-9 <andrei2006roman@gmail.com>
2026-05-20 20:17:29 +0300

    copiere fisiere din template pt sfml3

--- AplicatieGUI.cpp
+++ AplicatieGUI.cpp
@@ -1,39 +1,39 @@
-// #include "AplicatieGUI.h"
-//
-// AplicatieGUI::AplicatieGUI(PlatformaStreaming& p)
-//     : window(sf::VideoMode({1024, 768}), "Netflix C++"), platforma(p) {
-//
-//     window.setFramerateLimit(60);
-//
-//     if (!font.openFromFile("assets/fonts/Roboto.ttf")) {
-//         std::cerr << "Eroare: Nu s-a putut incarca fontul din assets/fonts/Roboto.ttf\n";
-//     }
-// }
-//
-// void AplicatieGUI::randeazaCatalog() {
-//     auto& catalog = platforma.getCatalogGlobal();
-//
-//     float pozitieY = 50.f;
-//     for (const auto& cv : catalog) {
-//         sf::Text text(font);
-//         text.setString(cv->getTitlu());
-//         text.setCharacterSize(20);
-//         text.setFillColor(sf::Color::White);
-//         text.setPosition({50.f, pozitieY});
-//
-//         window.draw(text);
-//         pozitieY += 40.f;
-//     }
-// }
-//
-// void AplicatieGUI::ruleaza() {
-//     while (window.isOpen()) {
-//         while (const std::optional event = window.pollEvent()) {
-//             if (event->is<sf::Event::Closed>()) window.close();
-//         }
-//
-//         window.clear(sf::Color(20, 20, 20));
-//         randeazaCatalog();
-//         window.display();
-//     }
-// }
+#include "AplicatieGUI.h"
+
+AplicatieGUI::AplicatieGUI(PlatformaStreaming& p)
+    : window(sf::VideoMode({1024, 768}), "Netflix C++"), platforma(p) {
+
+    window.setFramerateLimit(60);
+
+    if (!font.openFromFile("assets/fonts/Roboto.ttf")) {
+        std::cerr << "Eroare: Nu s-a putut incarca fontul din assets/fonts/Roboto.ttf\n";
+    }
+}
+
+void AplicatieGUI::randeazaCatalog() {
+    auto& catalog = platforma.getCatalogGlobal();
+
+    float pozitieY = 50.f;
+    for (const auto& cv : catalog) {
+        sf::Text text(font);
+        text.setString(cv->getTitlu());
+        text.setCharacterSize(20);
+        text.setFillColor(sf::Color::White);
+        text.setPosition({50.f, pozitieY});
+
+        window.draw(text);
+        pozitieY += 40.f;
+    }
+}
+
+void AplicatieGUI::ruleaza() {
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) window.close();
+        }
+
+        window.clear(sf::Color(20, 20, 20));
+        randeazaCatalog();
+        window.display();
+    }
+}
