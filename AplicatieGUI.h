39d2f1db05ef62e4b7fd10fbd47230e10c95ea26
Roman-9 <andrei2006roman@gmail.com>
2026-05-20 03:38:23 +0300

    testare eroare cppcheck

--- AplicatieGUI.h
+++ AplicatieGUI.h
@@ -1,17 +1,18 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include "PlatformaStreaming.h"
-
-
-class AplicatieGUI {
-private:
-    sf::RenderWindow window;
-    PlatformaStreaming& platforma;
-
-    sf::Font font;
-
-public:
-    explicit AplicatieGUI(PlatformaStreaming& p);
-    void ruleaza();
-    void randeazaCatalog();
-};
+// #pragma once
+// #include <SFML/Graphics.hpp>
+// #include "PlatformaStreaming.h"
+// #include <memory>
+//
+//
+// class AplicatieGUI {
+// private:
+//     sf::RenderWindow window;
+//     PlatformaStreaming& platforma;
+//
+//     sf::Font font;
+//
+// public:
+//     explicit AplicatieGUI(PlatformaStreaming& p);
+//     void ruleaza();
+//     void randeazaCatalog();
+// };
