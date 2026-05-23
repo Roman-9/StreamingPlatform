84d5652be0ddd767d928df298ca24c5289efc208
Roman-9 <andrei2006roman@gmail.com>
2026-05-23 16:45:21 +0300

    adaugare bloc try-catch global în main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include "PlatformaStreaming.h"
-
 #include "AplicatieGUI.h"
+#include "Exceptii.h"
 
 int main() {
-    std::cout << "=== INITIALIZARE NETFLIX PLATFORM ===\n\n";
+    try {
+        std::cout << "=== INITIALIZARE NETFLIX PLATFORM ===\n\n";
 
-    // 1. Initializam obiectul platformei legat de fisierul bazei de date
-    PlatformaStreaming platforma("streaming.db");
+        // 1. Initializam obiectul platformei legat de fisierul bazei de date
+        PlatformaStreaming platforma("streaming.db");
 
-    // 2. Lansam interfata grafica
-    std::cout << "\n[INFO] Lansam interfata grafica SFML...\n";
-    AplicatieGUI app(platforma);
-    app.ruleaza();
+        // 2. Lansam interfata grafica
+        std::cout << "\n[INFO] Lansam interfata grafica SFML...\n";
+        AplicatieGUI app(platforma);
+        app.ruleaza();
+    }
+    catch (const StreamingException& e) {
+        std::cerr << "[CRITICAL ERROR] Exceptie specifica platformei: " << e.what() << "\n";
+        return 1;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "[CRITICAL ERROR] Exceptie generala: " << e.what() << "\n";
+        return 2;
+    }
+    catch (...) {
+        std::cerr << "[CRITICAL ERROR] Exceptie necunoscuta!\n";
+        return 3;
+    }
 
     return 0;
 }
